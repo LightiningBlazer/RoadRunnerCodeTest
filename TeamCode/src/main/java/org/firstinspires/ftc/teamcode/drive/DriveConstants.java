07b169967a5c14bbad3ad0a6114b85a9b9c59a39
Yatharth Home <87783183+Deus-Ex-Machina-FLL@users.noreply.github.com>
2022-01-26 20:41:33 -0600

    Added TICKS_PER_REV as 537.7 And MAX_RPM as 312

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -20,10 +20,12 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 1;
-    public static final double MAX_RPM = 1;
+    public static final double TICKS_PER_REV = 537.7;
+    public static final double MAX_RPM = 312;
 
     /*
+            Inserted Those Values Someone Double check
+
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
      * Set this flag to false if drive encoders are not present and an alternative localization
      * method is in use (e.g., tracking wheels).
